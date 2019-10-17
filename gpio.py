146646f1f14ec4d28dbcbeb3a1cfd538e5526f78
Debaq <david.avila@uach.cl>
2019-10-17 09:59:44 -0300

    Update gpio.py

--- gpio.py
+++ gpio.py
@@ -5,10 +5,10 @@ pitft = PiTFT_GPIO()
 while True:
     if pitft.Button1:
         print "Button 1 pressed - screen off"
-        pitftf.Backlight(False)
+        #pitftf.Backlight(False)
     if pitft.Button2:
         print "Button 2 pressed - screen on"
-        pitft.Backlight(True) 
+        #pitft.Backlight(True) 
     if pitft.Button3:
         print "Button 3 pressed"
     if pitft.Button4:
